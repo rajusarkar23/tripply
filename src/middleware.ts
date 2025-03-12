ae6e646e1cb806897e5049664c04dccbbbf79d68
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-03-12 23:51:10 +0530

    add middleware for session

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (
+    request.nextUrl.pathname.startsWith("/admin") &&
+    !request.cookies.get("session")
+  ) {
+    return NextResponse.rewrite(
+      new URL("/authentication/admin/signin", request.url)
+    );
+  }
+}
