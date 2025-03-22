f6a3080d2ec4ef671efa593e2896ffd37c950f7c
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-03-22 13:26:01 +0530

    chnage cookie name in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   if (
     request.nextUrl.pathname.startsWith("/admin") &&
-    !request.cookies.get("session")
+    !request.cookies.get("_tripply_a_session_")
   ) {
     return NextResponse.rewrite(
       new URL("/authentication/admin/signin", request.url)
@@ -13,12 +13,12 @@ export function middleware(request: NextRequest) {
 
   if (
     request.nextUrl.pathname.startsWith("/profile") &&
-    !request.cookies.get("tousession")
+    !request.cookies.get("_tripply_tou_session_")
   ) {
     return NextResponse.rewrite(new URL("/authentication/signin", request.url));
   }
 
-  if (request.nextUrl.pathname.startsWith("/bookings") && !request.cookies.get("tousession")) {
+  if (request.nextUrl.pathname.startsWith("/bookings") && !request.cookies.get("_tripply_tou_session_")) {
     return NextResponse.rewrite(new URL("/authentication/signin", request.url))
   }
 }
